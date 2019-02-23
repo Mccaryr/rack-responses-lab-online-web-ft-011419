5bcbba40c5a0c69b18608a1c7ab9fee46410cded
Robert McCary <robert.mccary@yahoo.com>
2019-02-23 18:44:24 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if before noon
+      resp.write "Good Morning!"
+
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+end 
